16d583e76f2c7f045ccb82ce022bad12bd4e5d4a
annisadwiarum <annisadwiarum710@gmail.com>
2023-08-19 16:58:35 +0700

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,14 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Attendance;
 use App\Models\User;
+use App\Models\Attendance;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+          'name' => 'required',
+          'email' => 'required|email|unique:users',
+          'password' => 'required|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(responseFormatter(422, "Unprocessable Entity", "Validation fails.", $validator));
+        }
+
+         $user = User::create([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => Hash::make(request('password')),
+        ]);
+
+        if ($user) {
+            return response()->json(responseFormatter(200, 'OK', 'Success', $user));
+        } else {
+            return response()->json(responseFormatter(422, 'FAILED', 'Register Failed', $user));
+        }
+    }
+
     public function login(Request $request)
     {
         $error = basicValidation($request, [
@@ -17,7 +43,7 @@ class AuthController extends Controller
             'password' => 'required'
         ]);
 
-        if ($error) {
+          if ($error) {
             return response()->json(responseFormatter(422, "Unprocessable Entity", "Validation fails.", $error));
         }
 
@@ -33,7 +59,7 @@ class AuthController extends Controller
     public function test()
     {
         $user = User::first();
-        $attendance = Attendance::where('user_id', $user->id)->paginate(20);
+        $attendance = Attendance::where('user_id', $user->id)->paginate(10);
         return response()->json(responseFormatter(200, 'OK', 'Success', $attendance));
     }
 }
